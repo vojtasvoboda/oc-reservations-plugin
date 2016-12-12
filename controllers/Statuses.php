702391e70c0a6eadd9f7c93dc9502ca1fdbd0d2a
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2016-12-12 14:11:43 +0100

    Add reservations counter to mail confirmation

--- controllers/Statuses.php
+++ controllers/Statuses.php
@@ -6,8 +6,12 @@ use Flash;
 
 class Statuses extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\ReorderController'];
-    
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController',
+        'Backend\Behaviors\ReorderController',
+    ];
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
--- tests/facades/ReservationsFacadeTest.php
+++ tests/facades/ReservationsFacadeTest.php
@@ -124,6 +124,20 @@ class ReservationsFacadeTest extends PluginTestCase
         $model->storeReservation($data);
     }
 
+    public function testGetReservationsWithSameEmailCount()
+    {
+        $model = $this->getModel();
+
+        // create one reservation with test@test.cz email
+        $model->storeReservation($this->getTestingReservationData());
+
+        $count = $model->getReservationsWithSameEmailCount('vojtasvoboda.cz@gmail.com');
+        $this->assertEquals(0, $count);
+
+        $count = $model->getReservationsWithSameEmailCount('test@test.cz');
+        $this->assertEquals(1, $count);
+    }
+
     private function getTestingReservationData()
     {
         return [
